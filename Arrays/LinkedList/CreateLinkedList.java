50cf578a50bb7484a88a0ea5e84875192461f7b3
Amit Singh <amitkarun4559@gmail.com>
2023-01-19 09:55:08 +0530

    linkedlist

--- Arrays/LinkedList/CreateLinkedList.java
+++ Arrays/LinkedList/CreateLinkedList.java
@@ -245,6 +245,92 @@ public class CreateLinkedList {
         prev.next = null;
     }
 
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;// mid node
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergell = new Node(-1);
+        Node temp = mergell;
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergell.next;
+    }
+
+    public Node mergSort(Node head) {// O(nlogn)
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // find mid
+        Node mid = getMid(head);
+        // left & right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergSort(head);
+        Node newRight = mergSort(rightHead);
+        // merge
+        return merge(newLeft, newRight);
+    }
+
+    public void ZigZag() {
+        // Find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+        // reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        // alt merge- zig-zag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+            left = nextL;
+            right = nextR;
+        }
+    }
+
     public static void main(String[] args) {
         CreateLinkedList ll = new CreateLinkedList();
         // ll.print();
@@ -272,14 +358,25 @@ public class CreateLinkedList {
         // ll.deleteNthfromEnd(3);
         // ll.print();
         // System.out.println(ll.checkPalindrome());
-        head = new Node(1);
-        Node temp = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = temp;
-        System.out.println(isCycle());
-        removeCycle();
-        System.out.println(isCycle());
+        /*
+         * head = new Node(1);
+         * Node temp = new Node(2);
+         * head.next = temp;
+         * head.next.next = new Node(3);
+         * head.next.next.next = temp;
+         * System.out.println(isCycle());
+         * removeCycle();
+         * System.out.println(isCycle());
+         */
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        // ll.head = ll.mergSort(ll.head);
+        ll.ZigZag();
+        ll.print();
 
     }
 }
